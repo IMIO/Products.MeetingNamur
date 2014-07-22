e861d79d4d101fbd8047fe214111e72e3764ac67
André Nuyens <andre.nuyens@imio.be>
2014-07-22 16:05:44 +0000

    all test pass

--- src/Products/MeetingNamur/tests/testMeeting.py
+++ src/Products/MeetingNamur/tests/testMeeting.py
@@ -31,6 +31,13 @@ class testMeeting(MeetingNamurTestCase, mctm):
         Tests the Meeting class methods.
     """
 
+    def test_pm_RemoveOrDeleteLinkedItem(self):
+        """
+            Give temporary manager right to pmManager.
+        """
+        self.portal.acl_users.portal_role_manager.assignRoleToPrincipal('Manager', 'pmManager')
+        mctm.test_pm_RemoveOrDeleteLinkedItem(self)
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
