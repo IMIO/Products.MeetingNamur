0c616b78fd76ea55e411229a5e7370309dd20144
André Nuyens <andre.nuyens@imio.be>
2016-02-17 12:17:26 +0000

    Backported added missing test

--- src/Products/MeetingNamur/tests/testMeetingItem.py
+++ src/Products/MeetingNamur/tests/testMeetingItem.py
@@ -96,6 +96,18 @@ class testMeetingItem(MeetingNamurTestCase, mctmi):
         self.assertTrue(messages[0].message == ON_TRANSITION_TRANSFORM_TAL_EXPR_ERROR % ('decision',
                                                                                          "'some_wrong_tal_expression'"))
 
+    def test_subproduct_call_ItemStrikedAssembly(self):
+        self.test_pm_ItemStrikedAssembly()
+
+    def test_subproduct_call_Emergency(self):
+        self.test_pm_Emergency()
+
+    def test_subproduct_call_Completeness(self):
+        self.test_pm_Completeness()
+
+    def test_subproduct_call_SendItemToOtherMCManually(self):
+        self.test_pm_SendItemToOtherMCManually()
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
