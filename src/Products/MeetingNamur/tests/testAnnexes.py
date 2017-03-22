5170cda372deef0bb205806337f4f63cb19d3418
André Nuyens <andre.nuyens@imio.be>
2017-03-22 08:30:57 +0000

    one test again

--- src/Products/MeetingNamur/tests/testAnnexes.py
+++ src/Products/MeetingNamur/tests/testAnnexes.py
@@ -22,12 +22,47 @@
 # 02110-1301, USA.
 #
 
+from AccessControl import Unauthorized
+from DateTime import DateTime
 from Products.MeetingNamur.tests.MeetingNamurTestCase import MeetingNamurTestCase
 from Products.PloneMeeting.tests.testAnnexes import testAnnexes as pmta
 
 
 class testAnnexes(MeetingNamurTestCase, pmta):
     ''' '''
+
+
+    def test_pm_DecisionAnnexesDeletableByOwner(self):
+        """annexDecision may be deleted by the Owner, aka the user that added the annex."""
+        cfg = self.meetingConfig
+        self.changeUser('pmCreator1')
+        item = self.create('MeetingItem')
+        item.setDecision('<p>Decision</p>')
+        self.validateItem(item)
+        # when an item is 'accepted', the MeetingMember may add annexDecision
+        self.changeUser('pmManager')
+        meeting = self.create('Meeting', date=DateTime('2016/11/11'))
+        self.presentItem(item)
+        self.decideMeeting(meeting)
+        self.do(item, 'accept')
+        self.assertEqual(item.queryState(), 'accepted')
+        # creator can't add decision annex
+        self.changeUser('pmCreator1')
+        self.assertRaises(Unauthorized, self.addAnnex, item, relatedTo='item_decision')
+        # manager can
+        self.changeUser('pmManager')
+        decisionAnnex1 = self.addAnnex(item, relatedTo='item_decision')
+        self.assertTrue(decisionAnnex1 in item.objectValues())
+        # doable if cfg.ownerMayDeleteAnnexDecision is True
+        self.assertFalse(cfg.getOwnerMayDeleteAnnexDecision())
+        self.assertRaises(Unauthorized, item.restrictedTraverse('@@delete_givenuid'), decisionAnnex1.UID())
+        cfg.setOwnerMayDeleteAnnexDecision(True)
+        item.restrictedTraverse('@@delete_givenuid')(decisionAnnex1.UID())
+        self.assertFalse(decisionAnnex1 in item.objectValues())
+        # add an annex and another user having same groups for item can not remove it
+        decisionAnnex2 = self.addAnnex(item, relatedTo='item_decision')
+        self.changeUser('pmCreator1b')
+        self.assertRaises(Unauthorized, item.restrictedTraverse('@@delete_givenuid'), decisionAnnex2.UID())
 
 
 def test_suite():
